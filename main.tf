eb4603e74b959b0c8f8f4dd03b04a1b5713453ed
Jason Mills <jason.k.mills@gmail.com>
2025-01-16 08:09:16 -0500

    bucket init

--- main.tf
+++ main.tf
@@ -1,2 +1,8 @@
-terraform {
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+
 }
